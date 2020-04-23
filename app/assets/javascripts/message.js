571b0842b4f41c7375883285e4575f564aa3d499
perth330 <perth330@hotmail.com>
2020-04-23 15:12:02 +0900

    update-automatic-save

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="main_chat__message-list__table">
+       `<div class="main_chat__message-list__table" data-message-id=${message.id}>
           <div class="main_chat__message-list__table__top">
             <div class="main_chat__message-list__table__top__name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       var html =
-       `<div class="main_chat__message-list__table">
+       `<div class="main_chat__message-list__table" data-message-id=${message.id}>
           <div class="main_chat__message-list__table__top">
             <div class="main_chat__message-list__table__top__name">
               ${message.user_name}
@@ -39,27 +39,52 @@ $(function(){
       return html;
     };
   }
-$('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
+  $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.main_chat__message-list').append(html);
+        $('.main_chat__message-list').animate({ scrollTop: $('.main_chat__message-list')[0].scrollHeight});
+        $('.main_chat__message-form__new-message__submit').prop('disabled', false);
+        $('form')[0].reset();
+      })
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");      
+    });
+  })
+  var reloadMessages = function() {
+    var last_message_id = $('.main_chat__message-list__table:last').data("message-id");
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      url: "api/messages",
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+      data: {id: last_message_id}
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.main_chat__message-list').append(html);
-      $('.main_chat__message-list').animate({ scrollTop: $('.main_chat__message-list')[0].scrollHeight});
-      $('.main_chat__message-form__new-message__submit').prop('disabled', false);
-      $('form')[0].reset();
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main_chat__message-list').append(insertHTML);
+        $('.main_chat__message-list').animate({ scrollTop: $('.main_chat__message-list')[0].scrollHeight});
+      }
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
-  });
-})
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
